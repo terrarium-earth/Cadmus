99ff952d43dd319cdf9eea3e2d98e9e8e299a382
Abbie <abigail@ient.me>
2023-06-14 17:55:32 +0100

    Use MixinExtras @Local for local capture

--- common/src/main/java/earth/terrarium/cadmus/mixin/common/xaero/WorldMapSessionMixin.java
+++ common/src/main/java/earth/terrarium/cadmus/mixin/common/xaero/WorldMapSessionMixin.java
@@ -1,5 +1,6 @@
 package earth.terrarium.cadmus.mixin.common.xaero;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import earth.terrarium.cadmus.common.compat.xaero.XaerosCompat;
 import net.minecraft.client.multiplayer.ClientPacketListener;
 import org.spongepowered.asm.mixin.Mixin;
@@ -9,16 +10,6 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import xaero.map.WorldMapSession;
-import xaero.map.biome.BiomeColorCalculator;
-import xaero.map.biome.BiomeGetter;
-import xaero.map.cache.BlockStateShortShapeCache;
-import xaero.map.cache.BrokenBlockTintCache;
-import xaero.map.executor.Executor;
-import xaero.map.file.MapSaveLoad;
-import xaero.map.file.worldsave.WorldDataHandler;
-import xaero.map.file.worldsave.WorldDataReader;
-import xaero.map.file.worldsave.biome.WorldDataBiomeManager;
-import xaero.map.graphics.TextureUploader;
 import xaero.map.highlight.HighlighterRegistry;
 
 @Pseudo
@@ -29,7 +20,7 @@ public abstract class WorldMapSessionMixin {
             at = @At(value = "INVOKE", target = "Lxaero/map/highlight/HighlighterRegistry;end()V"),
             locals = LocalCapture.CAPTURE_FAILSOFT
     )
-    private void cadmus$registerHighlighters(ClientPacketListener connection, long biomeZoomSeed, CallbackInfo ci, BlockStateShortShapeCache blockStateShortShapeCache, MapSaveLoad mapSaveLoad, TextureUploader textureUploader, BiomeGetter biomeGetter, BrokenBlockTintCache brokenBlockTintCache, BiomeColorCalculator biomeColorCalculator, WorldDataBiomeManager worldDataBiomeManager, WorldDataReader worldDataReader, Executor worldDataRenderExecutor, WorldDataHandler worldDataHandler, HighlighterRegistry highlightRegistry) {
+    private void cadmus$registerHighlighters(ClientPacketListener connection, long biomeZoomSeed, CallbackInfo ci, @Local HighlighterRegistry highlightRegistry) {
         XaerosCompat.registerHighlighters(highlightRegistry);
     }
 }
